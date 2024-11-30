c30b47078ed26212b98e5afd440de9cc2cd5d75f
yifanyang21 <yfyang167@gmail.com>
2024-11-30 16:13:51 +0100

    tryagain

--- map.js
+++ map.js
@@ -255,7 +255,7 @@ async function initMap() {
         select.appendChild(option);
     });
 
-    streetNetworkData = await loadGeoJson('https://github.com/yifanyang21/mindtheheat/raw/refs/heads/main/data/final_score_heat2.geojson');
+    streetNetworkData = await loadGeoJson('https://github.com/yifanyang21/mindtheheat/blob/main/data/final_score_heat2.geojson?raw=true');
     clusterData = await loadGeoJson('data/gdf_simple_clusters.geojson');
 
     updateMap('all');
