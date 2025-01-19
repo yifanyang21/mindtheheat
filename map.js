473d1171b0fe69f2e380d2ac2c7cd3a8ea5c190b
yifanyang21 <yfyang167@gmail.com>
2025-01-19 20:27:23 +0100

    fix_copy_error

--- map.js
+++ map.js
@@ -16,12 +16,12 @@ function createTileLayer(url, attribution) {
     return L.tileLayer(url, { attribution });
 }
 
-let lightTileLayer = createTileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-});
-let darkTileLayer = createTileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-});
+let lightTileLayer = createTileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', 
+    '&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+);
+let darkTileLayer = createTileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', 
+    '&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+);
 
 lightTileLayer.addTo(map);
 
